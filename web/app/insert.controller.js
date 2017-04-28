3705e040d8634d5b84309fb623511c6fa1a68467
Unknown <michele.righetti1998@gmail.com>
2017-04-28 20:29:14 +0200

    implementata chiamata a server per inserimento evento

--- web/app/insert.controller.js
+++ web/app/insert.controller.js
@@ -31,43 +31,51 @@ app
          * sends event information to server
          */
         $scope.insert = function () {
+            
+            /*
+                descrizione dell'evento = descrizione,
+                giorno = day,
+                ora di inizio = oraInizio,
+                ora di fine = oraFine,
+                classi = classi,
+                stanze = stanze
+            */
+
             var desc = $scope.event.description;
             var day = $scope.event.day.getFullYear() + "-" + ($scope.event.day.getMonth()+1) + "-" + $scope.event.day.getDate();
             var hStart = $scope.event.hourStart.getHours() + ":" + $scope.event.hourStart.getMinutes();
             var hEnd = $scope.event.hourEnd.getHours() + ":" + $scope.event.hourEnd.getMinutes();
-            var sClassesToString = $scope.event.classes.toString().replace(/,(?=[^\s])/g, ", ");
-            var sRoomsToString = $scope.event.rooms.toString().replace(/,(?=[^\s])/g, ", ");
+            var sClasses = $scope.event.classes; //$scope.event.classes.toString().replace(/,(?=[^\s])/g, ", ");
+            var sRooms = $scope.event.rooms; //$scope.event.rooms.toString().replace(/,(?=[^\s])/g, ", ");
             var data = "descrizione="+desc+"&day="+day+"&oraInizio="+hStart+"&oraFine="+hEnd+"&classi="
-                +sClassesToString+"&stanze="+sRoomsToString+"&token="+$rootScope.token;
+                +sClasses+"&stanze="+sRooms+"&token="+$rootScope.token;
 
             var req = {
                 method: 'POST',
-                //url: 'http://'+CONFIG.HOST+':8080/api/events/' + $scope.event.date.getFullYear() + "/" + $scope.event.date.getMonth() + "/" + $scope.event.date.getDate(),
+                url: 'http://'+CONFIG.HOST+':8080/api/creaEvento',
                 data: data,
                 headers: {
                     'Content-Type': 'application/x-www-form-urlencoded'
                 }
             };
+
             console.log(req.data);
-            $scope.event = {};
-            $scope.event.day = new Date();
-            $scope.event.hourStart = new Date();
-            $scope.event.hourEnd = new Date();
-            $scope.event.classes = [];
-            $scope.event.rooms = [];
-            /*
+            
             $http(req)
                 .then(
                     function(data) {
                         $mdToast.show($mdToast.simple().textContent("Evento inserito correttamente"));
                         $scope.event = {};
+                        $scope.event.day = new Date();
+                        $scope.event.hourStart = new Date();
+                        $scope.event.hourEnd = new Date();
+                        $scope.event.classes = [];
+                        $scope.event.rooms = [];
                     },
                     function(err) {
                         $mdToast.show($mdToast.simple().textContent("Errore di rete: "+ err));
                     }
                 );
-            */
-
         }
 
 
