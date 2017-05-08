b22fd0e93264f8180a828eb105527cc6f3625a57
Michele Righetti <michele.righetti1998@gmail.com>
2017-05-08 15:16:40 +0200

    aggiunta richiesta di conferma per liberare la risorsa, correzione chiamata liberaRisorsE

--- web/app/liberarisorse.controller.js
+++ web/app/liberarisorse.controller.js
@@ -1,5 +1,5 @@
 app
-    .controller('LiberaRisorseCtrl', function($scope, $timeout, $mdSidenav, $log, $filter, $http, $q, $mdToast, $element, $rootScope, $httpParamSerializerJQLike, CONFIG) {
+    .controller('LiberaRisorseCtrl', function($scope, $timeout, $mdSidenav, $log, $filter, $http, $q, $mdToast, $element, $rootScope, $httpParamSerializerJQLike, CONFIG, $mdDialog) {
 
         $scope.teachers;
         $scope.classes;
@@ -29,46 +29,43 @@ app
          * sends event information to server
          */
         $scope.insert = function () {
-            
-            /*
-                descrizione dell'evento = descrizione,
-                giorno = day,
-                ora di inizio = oraInizio,
-                ora di fine = oraFine,
-                classi = classi,
-                stanze = stanze
-            */
+            var confirm = $mdDialog.confirm()
+              .textContent('La risorsa verrà liberata per sempre. Continuare?')
+              .ok('CONFERMA')
+              .cancel('ANNULLA');
 
-            var desc = $scope.event.description;
-            var day = $scope.event.day.getFullYear() + "-" + ($scope.event.day.getMonth()+1) + "-" + $scope.event.day.getDate();
-            var sClass = $scope.event.class; //$scope.event.class.toString().replace(/,(?=[^\s])/g, ", ");
-            var sOre = $scope.event.ore; //$scope.event.ore.toString().replace(/,(?=[^\s])/g, ", ");
-            var data = "descrizione="+desc+"&day="+day+"&classe="
-                +sClass+"&ore="+sOre+"&token="+$rootScope.token;
+            $mdDialog.show(confirm).then(function() {
+                var desc = $scope.event.description;
+                var day = $scope.event.day.getFullYear() + "-" + ($scope.event.day.getMonth()+1) + "-" + $scope.event.day.getDate();
+                var sClass = $scope.event.class; //$scope.event.class.toString().replace(/,(?=[^\s])/g, ", ");
+                var sOre = $scope.event.ore; //$scope.event.ore.toString().replace(/,(?=[^\s])/g, ", ");
+                var data = "descrizione="+desc+"&day="+day+"&classe="
+                    +sClass+"&ore="+sOre+"&token="+$rootScope.token;
 
-            var req = {
-                method: 'POST',
-                url: 'http://'+CONFIG.HOST+':8080/api/liberaRisorsa',
-                data: data,
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                }
-            };
-
-            console.log(req.data);
-            
-            $http(req)
-                .then(
-                    function(data) {
-                        $mdToast.show($mdToast.simple().textContent("Liberazione avvenuta con successo correttamente"));
-                        $scope.event = {};
-                        $scope.event.day = new Date();
-                        $scope.event.ore = [];
-                    },
-                    function(err) {
-                        $mdToast.show($mdToast.simple().textContent("Errore di rete: "+ err));
+                var req = {
+                    method: 'POST',
+                    url: 'http://'+CONFIG.HOST+':8080/api/liberaRisorse',
+                    data: data,
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded'
                     }
-                );
+                };
+
+                console.log(req.data);
+
+                $http(req)
+                    .then(
+                        function(data) {
+                            $mdToast.show($mdToast.simple().textContent("Liberazione avvenuta con successo"));
+                            $scope.event = {};
+                            $scope.event.day = new Date();
+                            $scope.event.ore = [];
+                        },
+                        function(err) {
+                            $mdToast.show($mdToast.simple().textContent("Errore di rete: "+ err));
+                        }
+                    );
+            });
         }
 
 
